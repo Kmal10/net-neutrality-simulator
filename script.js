e95f616c5718bb12a00cd5bf25c080b1fec1e04a
Kabir <malikk2003@gmail.com>
2018-02-24 14:21:11 -0500

    Updated the code

--- script.js
+++ script.js
@@ -1,5 +1,105 @@
 document.querySelector('.beginSimulation').addEventListener('click', function() {
-  window.alert('Hey Window, Save Net Neutrality');
-  document.querySelector('body').style.background = 'gold';
-}
+  window.alert('Save Net Neutrality!');
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  document.querySelector('body').style.background = 'yellow';
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  document.querySelector('.beginSimulation').style.fontFamily = "impact";
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  document.querySelector('.loadingArticle').textContent = "Loading...";
+  document.querySelector('.loadingArticle').href = "none";
+
+  });
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  document.querySelector('.steph').src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var articleParagraphs = document.querySelectorAll('p');
+  articleParagraphs[0].style.color = "white";
+  articleParagraphs[0].style.opacity = "0.6";
+  articleParagraphs[0].style.fontFamily = "monaco";
+  articleParagraphs[1].style.color = "white";
+  articleParagraphs[1].style.opacity = "0.6";
+  articleParagraphs[1].style.fontFamily = "monaco";
+  articleParagraphs[2].style.color = "white";
+  articleParagraphs[2].style.opacity = "0.6";
+  articleParagraphs[2].style.fontFamily = "monaco";
+  articleParagraphs[3].style.color = "white";
+  articleParagraphs[3].style.opacity = "0.6";
+  articleParagraphs[3].style.fontFamily = "monaco";
+  articleParagraphs[4].style.color = "white";
+  articleParagraphs[4].style.opacity = "0.6";
+  articleParagraphs[4].style.fontFamily = "monaco";
+  articleParagraphs[5].style.color = "white";
+  articleParagraphs[5].style.opacity = "0.6";
+  articleParagraphs[5].style.fontFamily = "monaco";
+
+  document.querySelector('.beginSimulation').style.opacity = "0.6";
+});
+
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var links = document.querySelectorAll('a');
+  links[0].href = "none";
+  links[1].href = "none";
+  links[2].href = "none";
+  links[3].href = "none";
+  links[4].href = "none";
+  links[5].href = "none";
+  links[6].href = "none";
+  links[7].href = "none";
+
+});
+
+
+
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var titles = document.querySelectorAll('h3');
+  titles[0].textContent = "Loading...";
+  titles[1].textContent = "Loading...";
+  titles[2].textContent = "Loading...";
+  titles[3].textContent = "Loading...";
+  titles[4].textContent = "Loading...";
+  titles[5].textContent = "Loading...";
+  titles[6].textContent = "Loading...";
+  titles[7].textContent = "Loading...";
+
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var images = document.querySelectorAll('img');
+  images[14].src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+  images[15].src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var articleParagraphs = document.querySelectorAll('p');
+  articleParagraphs[0].style.opacity = "0.5";
+  articleParagraphs[1].style.opacity = "0.5";
+  articleParagraphs[2].style.opacity = "0.5";
+  articleParagraphs[3].style.opacity = "0.5";
+  articleParagraphs[4].style.opacity = "0.5";
+  articleParagraphs[5].style.opacity = "0.5";
+});
+
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var titles = document.querySelectorAll('h3');
+  titles[0].style.color = "blue";
+});
+document.querySelector('.beginSimulation').addEventListener('click', function() {
+  var videos = document.querySelectorAll('iframe');
+  videos[0].src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+  videos[1].src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+  videos[2].src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+  videos[3].src = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
+});
+document.querySelector('.button').addEventListener('click', function() {
+var userName = document.querySelector('.inputName').value;
+document.querySelector('.userNameHere').appendChild(inputName);
+var userEmail = document.querySelector('.inputEmail').value;
+document.querySelector('.userEmailHere').appendChild(inputName);
+var userComment = document.querySelector('.inputComment').value;
+document.querySelector('.userNameHere').appendChild(inputName);
+
 });
